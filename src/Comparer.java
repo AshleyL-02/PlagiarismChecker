b855ebac4f80afed71253096888e63920ef41d3a
Ashley Luty <Ashley Luty@LAPTOP-GS8OU5DR.home>
2020-11-30 17:53:07 -0800

    - added comparer and n-gram comparison method

---
+++ src/Comparer.java
@@ -0,0 +1,28 @@
+import java.util.Set;
+
+//program should figure out if I'm quoting shakespeare?
+
+// Comparer finds n-gram equality between two document n-grams
+public class Comparer {
+    
+    // pre: takes two sets of strings representing the n-grams of two documents
+    // post: returns number of n-grams in common
+    public static int compareNGrams(Set<String> ngrams1, Set<String> ngrams2) {
+        // choose smaller set
+        Set<String> ngrams = ngrams1;
+        Set<String> ngramsOther = ngrams2;
+        if(ngrams2.size() < ngrams1.size()) {
+            ngrams = ngrams2;
+            ngramsOther = ngrams1;
+        }
+        
+        // iterate over smaller set
+        int counter = 0;
+        for(String ngram : ngrams) {
+            if(ngramsOther.contains(ngram)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        Converter c = new Converter("i love dOGs so, much. don't dude YEA");
+        Converter d = new Converter("I LOVE dogs so much!! They're cool. I don't dude YEA");
+        
+        System.out.println(c.getNGrams().size() + ", " + d.getNGrams().size());
+        System.out.println(Comparer.compareNGrams(c.getNGrams(), d.getNGrams()));
     }
 }
