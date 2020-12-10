90f9286bfee48ca39b9b0afe662b3359f290d2d3
Ashley Luty <Ashley Luty@LAPTOP-GS8OU5DR.home>
2020-12-09 17:41:42 -0800

    - smth

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -1,27 +1,24 @@
 //Group 29
 //Joshua Wang, Ashley Luty, Emma Chen
-// A PlagiarismChecker checks the similarity between two texts. When the class is run, the user interface should pop up.
+// A PlagiarismChecker checks the similarity between two texts provided via user interface.
 
 import java.awt.EventQueue;
 import java.io.FileNotFoundException;
 
 public class PlagiarismChecker {
-    //application window
-    private static WindowUI appWindow;
-
     public static void main(String[] args) throws FileNotFoundException { 
         // called once, to parse synonyms file
         SynonymMap.setupMap();
         
-        createAppWindow();
+        setupAppWindow();
     }
     
-    private static void createAppWindow() {        
-        // create application window
+    // post: constructor helper method, creates and opens application window
+    private static void setupAppWindow() {        
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
-                    appWindow = new WindowUI();
+                    WindowUI appWindow = new WindowUI();
                     appWindow.setVisible(true);
                 } catch (Exception e) {
                     e.printStackTrace();
