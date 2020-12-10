eeac2a362cf88b3d3c60dd870f13e32f64076b83
Ashley Luty <Ashley Luty@LAPTOP-GS8OU5DR.home>
2020-12-09 16:41:18 -0800

    - removed extra Comparer fields

--- src/Comparer.java
+++ src/Comparer.java
@@ -1,70 +1,58 @@
 //Group 29
 //Ashley Luty, Joshua Wang, Emma Chen
-//The Comparer class does the comparison between two documents and returns a value that represents the
-//percent similarity of the first document to the second document (n-gram equality)
+
+//The Comparer class calculates the percent similarity of two documents (n-gram equality),
+// and the synonym similarity
 
 import java.util.*;
 
 public class Comparer {
-
-    //Set<String> of n-grams for the document that is being suspected for plagiarism
-    private Set<String> ngrams1;
-
-    //Set<String> of n-grams for the document that the other document is being suspected for plagiarizing
-    //off of
-    private Set<String> ngrams2;
-
-    //approximate score (out of 100.00) of the amount of text of the first document that is copied from the
-    //second
+    //approximate percent (out of 100.00) of the amount of text the first document copied from the second
     private double exactScore;
 
-    //approximate score (out of 100.00) of how SIMILAR (not just copied text) the two documents are to each
-    //other
+    //approximate percent (out of 100.00) of the amount of text the first document copied from the second,
+    //  that contain matching (but not equal) synonyms
     private double synonymScore;
 
     //pre: takes in the Converter of two documents (the first is for the document that is being suspected for
-    //plagiarism and the second is for the document that the other document is being suspected for plagiarizing
-    //off of
-    //
-    //post: initializes the two n-gram Set<String> for both documents
+    //  plagiarism and the second is for the document that the other document is being suspected for plagiarizing
+    //  off of)
+    //post: initializes the two similarity scores for the documents, exact and synonym-only scores
     public Comparer(Converter c1, Converter c2) {
-        ngrams1 = c1.getNGrams();
-        ngrams2 = c2.getNGrams();
-
-        compareNGrams();
+        compareNGrams(c1.getNGrams(), c2.getNGrams());
     }
 
     //post: returns a double score (percent out of 100.00) that is number of n-grams in that are exactly the same
     //between the two documents divided by the total number of n-grams of the document
     //suspected of plagiarism. also initializes the (double) synonymScore field
     // if either ngram set is empty, scores are 0.0%
-    private void compareNGrams() {
+    private void compareNGrams(Set<String> ngramsMain, Set<String> ngramsCompared) {
         // if either ngram set is empty, scores are 0
-        if(ngrams1.isEmpty() || ngrams2.isEmpty()) {
+        if(ngramsMain.isEmpty() || ngramsCompared.isEmpty()) {
             exactScore =0;
             synonymScore =0;
         } else {
             // choose smaller set
-            Set<String> ngrams = ngrams1;
-            Set<String> ngramsOther = ngrams2;
-            if (ngrams2.size() < ngrams1.size()) {
-                ngrams = ngrams2;
-                ngramsOther = ngrams1;
+            Set<String> ngrams = ngramsMain;
+            Set<String> ngramsOther = ngramsCompared;
+            if (ngramsCompared.size() < ngramsMain.size()) {
+                ngrams = ngramsCompared;
+                ngramsOther = ngramsMain;
             }
 
             // iterate over smaller set
-            int copied = 0;
-            int similar = 0;
+            int exactMatches = 0;
+            int synonymMatches = 0; //synonym-only
             for (String ngram : ngrams) {
                 if (ngramsOther.contains(ngram)) {
-                    copied++;
+                    exactMatches++;
                     ngramsOther.remove(ngram);
                 } else {
                     Set<String> toRemove = new HashSet<String>();
                     for (String ngramOther : ngramsOther) {
                         if (SynonymMap.synonymInSet(ngram, ngramOther)) {
                             toRemove.add(ngramOther);
-                            similar++;
+                            synonymMatches++;
                         }
                     }
                     for (String remove : toRemove) {
@@ -73,8 +61,8 @@ public class Comparer {
                 }
             }
 
-            synonymScore = (double) ((int) (((double) similar / (double) ngrams1.size() * 100.00) * 100.00) / 100.00);
-            exactScore = (double) ((int) (((double) copied / (double) ngrams1.size() * 100.00) * 100.00) / 100.00);
+            synonymScore = (double) ((int) (((double) synonymMatches / (double) ngramsMain.size() * 100.00) * 100.00) / 100.00);
+            exactScore = (double) ((int) (((double) exactMatches / (double) ngramsMain.size() * 100.00) * 100.00) / 100.00);
         }
     }
     
